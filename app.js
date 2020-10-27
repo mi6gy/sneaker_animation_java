59e1d3ed6c67b283297e5a9e01b173714b98e602
Miguel Jimenez <mjimenez08@gmail.com>
2020-10-27 16:46:09 -0400

    added event listener for animations for hover and mouse leave

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container'); 
 
+ //Event Animetion
+ container.addEventListener("mousemove", (e) =>{
+    //  console.log(e.pageX, e.pageY);
+
+     let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+     let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+
+     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+ });
+
+ //animate return
+
+ container.addEventListener("mouseenter", (e) =>{
+     card.style.transition = "none";
+ })
+ constainer.addEventListener("mouseleave", (e) =>{
+     card.style.transition = "all 0.5s ease";
+     card.style.transform = `rotateY(0deg) rotateX(odeg`;
+ }) ;
+
+ 
